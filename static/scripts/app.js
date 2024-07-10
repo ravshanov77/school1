b281111f97f18d098b0edc862a0463444b7a3829
Farrukh <farruxlynx@gmail.com>
2024-07-10 20:42:06 +0500

    Initial commit

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,23 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Load scroll position after content is loaded
-  if (localStorage.getItem("scrollPosition")) {
-    console.log(
-      "Restoring scroll position:",
-      localStorage.getItem("scrollPosition")
-    );
-    window.scrollTo(0, parseInt(localStorage.getItem("scrollPosition")));
-  } else {
-    console.log("No scroll position saved");
-  }
+window.addEventListener("beforeunload", function () {
+  sessionStorage.setItem("scrollPosition", window.scrollY);
 });
 
-window.addEventListener("beforeunload", function () {
-  localStorage.setItem(
-    "scrollPosition",
-    window.scrollY || document.documentElement.scrollTop
-  );
-  console.log(
-    "Saved scroll position:",
-    window.scrollY || document.documentElement.scrollTop
-  );
+// Restore the scroll position when the page loads
+window.addEventListener("DOMContentLoaded", function () {
+  const scrollPosition = sessionStorage.getItem("scrollPosition");
+  if (scrollPosition) {
+    window.scrollTo(0, parseInt(scrollPosition));
+  }
 });
