be593ff92f1e2317c763a80eed7cbc39c1556239
Farrukh <farruxlynx@gmail.com>
2024-07-10 20:45:09 +0500

    Initial commit

---
+++ static/scripts/swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiper-container", {
+  loop: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
